5f195435cd94bf2d66df2387d61e5f7e018cd5e8
White, Eric <ewhite@ciena.com>
2016-03-07 11:50:39 -0500

    Change demo to component helper and make compatible to for Frost-Guide

--- tests/dummy/app/pods/demo/controller.js
+++ tests/dummy/app/pods/demo/controller.js
@@ -1,24 +1,33 @@
 import Ember from 'ember'
+import config from '../../config/environment'
 
 export default Ember.Controller.extend({
   selectedItems: Ember.A(),
-
-  actions: {
-    selected (attrs) {
-      if (attrs.isSelected) {
-        this.get('selectedItems').addObject(attrs.record)
+  componentPath: Ember.computed({
+    get() {
+      if (config.isFrostGuideDirectory) {
+        return "user-list-item"
       } else {
-        this.get('selectedItems').removeObject(attrs.record)
+        return "demo/user-list-item"
       }
-    },
-
-    yEndReached () {
-      this.notifications.addNotification({
-        message: 'Scroll reached end of y axis',
-        type: 'success',
-        autoClear: true,
-        clearDuration: 2000
-      })
     }
+  }),
+  actions: {
+    selected(attrs) {
+        if (attrs.isSelected) {
+          this.get('selectedItems').addObject(attrs.record)
+        } else {
+          this.get('selectedItems').removeObject(attrs.record)
+        }
+      },
+
+      yEndReached() {
+        this.notifications.addNotification({
+          message: 'Scroll reached end of y axis',
+          type: 'success',
+          autoClear: true,
+          clearDuration: 2000
+        })
+      }
   }
 })
