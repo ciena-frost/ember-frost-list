c659fefc2f75fc54ab1b4a62f139f2e63594f505
Matthew Dahl <mdahl@ciena.com>
2016-03-14 07:31:00 -0700

    Add code coverage

---
+++ tests/unit/components/frost-list-item-test.js
@@ -0,0 +1,36 @@
+const expect = chai.expect
+
+import {beforeEach, describe, it} from 'mocha'
+import FrostListItem from 'ember-frost-list/pods/components/frost-list-item/component'
+
+describe('frost-list-item', function () {
+  let component
+
+  beforeEach(function () {
+    component = FrostListItem.create()
+  })
+
+  describe('when model.isSelected is true', function () {
+    beforeEach(function () {
+      component.set('model', {
+        isSelected: true
+      })
+    })
+
+    it('isSelected returns true', function () {
+      expect(component.get('isSelected')).to.be.truthy
+    })
+  })
+
+  describe('when model.isSelected is false', function () {
+    beforeEach(function () {
+      component.set('model', {
+        isSelected: true
+      })
+    })
+
+    it('isSelected returns false', function () {
+      expect(component.get('isSelected')).to.be.falsy
+    })
+  })
+})
