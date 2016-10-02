5449fb585cfd4d0b0402fc302c6d04b48a3d555c
Steven Glanzer <sglanzer@ciena.com>
2016-10-02 14:34:13 -0400

    Upgrading to ember-frost-sort 3.0

--- blueprints/ember-frost-list/index.js
+++ blueprints/ember-frost-list/index.js
@@ -2,8 +2,8 @@ module.exports = {
   afterInstall: function () {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-frost-core', target: '>=0.0.13 <2.0.0'},
-        {name: 'ember-frost-sort', target: '>=1.0.0 <2.0.0'},
+        {name: 'ember-frost-core', target: '>=0.0.13 <1.0.0'},
+        {name: 'ember-frost-sort', target: '>=3.0.0 <4.0.0'},
         {name: 'smoke-and-mirrors', target: '>=0.5.4 <1.0.0'},
         {name: 'ember-prop-types', target: '>=2.4.0 < 3.0.0'}
       ]
