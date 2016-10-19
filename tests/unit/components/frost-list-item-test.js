fc466f9d5a5ceed310f925342ca6bf9de134969f
Jasonjys <jasonjys0511@gmail.com>
2016-10-19 16:00:56 -0400

    created unit tests for frost-list-item

---
+++ tests/unit/components/frost-list-item-test.js
@@ -0,0 +1,63 @@
+import {expect} from 'chai'
+import Ember from 'ember'
+const {run} = Ember
+import {describeComponent} from 'ember-mocha'
+import {
+  beforeEach,
+  describe,
+  it
+} from 'mocha'
+
+describeComponent(
+  'frost-list-item',
+  'Unit: FrostListItemComponent',
+  {
+    unit: true
+  },
+  function () {
+    let component
+
+    beforeEach(function () {
+      component = this.subject()
+    })
+
+    it('includes className frost-list-item', function () {
+      expect(component.classNames).to.include('frost-list-item')
+    })
+
+    it('sets dependent keys correctly', function () {
+      const isSelectedDependentKeys = [
+        'model.isSelected'
+      ]
+
+      const isExpandedDependentKeys = [
+        'model.isExpanded'
+      ]
+
+      expect(
+        component.isSelected._dependentKeys,
+        'Dependent keys are correct for _records computed property'
+      ).to.eql(isSelectedDependentKeys)
+
+      expect(
+        component.isExpanded._dependentKeys,
+        'Dependent keys are correct for _hasHeader computed property'
+      ).to.eql(isExpandedDependentKeys)
+    })
+
+    describe('"isExpanded" computed property', function () {
+      it('is set correctly', function () {
+        let model = {
+          isExpanded: true
+        }
+
+        run(() => { component.set('model', model) })
+
+        expect(
+          component.get('isExpanded'),
+          'isExpanded: "true"'
+        ).to.be.true
+      })
+    })
+  }
+)
