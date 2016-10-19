80c4ec8b3b72432f7ca231a824d2ba05bf56cd5a
Jasonjys <jasonjys0511@gmail.com>
2016-10-19 13:10:16 -0400

    created unit tests for frost-list-expension

---
+++ tests/unit/components/frost-list-expansion-test.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai'
+import {describeComponent} from 'ember-mocha'
+import {
+  beforeEach,
+  it
+} from 'mocha'
+
+describeComponent(
+  'frost-list-expansion',
+  'Unit: FrostListExpansionComponent',
+  {
+    unit: true
+  },
+  function () {
+    let component
+
+    beforeEach(function () {
+      component = this.subject()
+    })
+
+    it('includes className frost-list-expansion', function () {
+      expect(component.classNames).to.include('frost-list-expansion')
+    })
+  }
+)
