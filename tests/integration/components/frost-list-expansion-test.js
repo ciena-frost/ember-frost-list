f1b2b61077ba624320f9722b1188d7bf9232a118
Jasonjys <jasonjys0511@gmail.com>
2016-10-25 12:56:12 -0400

    added integration test for onCollapseAll and onExpandAll

--- tests/integration/components/frost-list-expansion-test.js
+++ tests/integration/components/frost-list-expansion-test.js
@@ -1,10 +1,15 @@
 import { expect } from 'chai'
+import {
+  $hook,
+  initialize
+} from 'ember-hook'
 import Ember from 'ember'
 import {
   describeComponent,
   it
 } from 'ember-mocha'
 import hbs from 'htmlbars-inline-precompile'
+import { beforeEach } from 'mocha'
 import sinon from 'sinon'
 
 describeComponent(
@@ -14,6 +19,10 @@ describeComponent(
     integration: true
   },
   function () {
+    beforeEach(function () {
+      initialize()
+    }) 
+
     it('renders', function () {
       const targetObject = {
         onCollapseAll: sinon.spy(),
@@ -35,22 +44,42 @@ describeComponent(
     })
 
     it('fires onCollapseAll closure action', function () {
-      const externalActionSpy = sinon.spy()
+      const collapseAllSpy = sinon.spy()
 
-      this.on('externalAction', externalActionSpy)
+      this.on('collapseAllAction', collapseAllSpy)
 
       this.render(hbs`
         {{frost-list-expansion
-          onCollapseAll=(action 'externalAction')
-          onExpandAll=(action 'externalAction')
+          onCollapseAll=(action 'collapseAllAction')
+          onExpandAll='onExpandAll'
         }}
       `)
 
-      this.$('.frost-list-expansion-item').trigger('click')
+      this.$($hook('collapse')).trigger('click')
 
       expect(
-        externalActionSpy.called,
+        collapseAllSpy.called,
         'onCollapseAll is fired'
+      ).to.be.true
+    })
+
+    it('fires onExpandAll closure action', function () {
+      const expandAllSpy = sinon.spy()
+
+      this.on('expandAllAction', expandAllSpy)
+
+      this.render(hbs`
+        {{frost-list-expansion
+          onCollapseAll='onCollapseAll'
+          onExpandAll=(action 'expandAllAction')
+        }}
+      `)
+
+      this.$($hook('expand')).trigger('click')
+
+      expect(
+        expandAllSpy.called,
+        'onExpandAll is fired'
       ).to.be.true
     })
   }
