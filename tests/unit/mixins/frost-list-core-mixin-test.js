93f52076f0e80b77701a6a1d7944121f5928f51d
Jasonjys <jasonjys0511@gmail.com>
2016-11-01 10:28:07 -0400

    created test for frost-list-core-mixin

---
+++ tests/unit/mixins/frost-list-core-mixin-test.js
@@ -0,0 +1,68 @@
+import { expect } from 'chai'
+import {
+  beforeEach,
+  describe,
+  it
+} from 'mocha'
+import Ember from 'ember'
+const {on} = Ember
+import FrostListCoreMixin from 'ember-frost-list/mixins/frost-list-core-mixin'
+
+describe('FrostListCoreMixin', function () {
+  let controller
+  let listConfig
+  const testItems = [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    }
+  ]
+  beforeEach(function () {
+    listConfig = {
+      items: 'model',
+      component: 'user-list-item'
+    }
+    controller = Ember.Controller.extend(FrostListCoreMixin).create({
+      initListCoreMixin: on('init', function () {
+        Ember.defineProperty(this, listConfig.items, undefined, testItems)
+        Ember.defineProperty(this, '_listItems', Ember.computed.alias(listConfig.items))
+      })
+    })
+  })
+
+  it('works', function () {
+    let testObject = Ember.Object.extend(FrostListCoreMixin)
+    let subject = testObject.create({
+      initListCoreMixin: on('init', function () {
+        Ember.defineProperty(this, '_listItems', Ember.computed.alias(listConfig.items))
+      })
+    })
+    expect(
+      subject
+    ).to.be.ok
+  })
+
+  it('filteredItems computed property', function () {
+    // console.log(controller)
+    // console.log(controller.model)
+    // console.log(controller._listItems)
+    // console.log(controller.get('listItems'))
+    expect(
+      controller.get('filteredItems'),
+      'listConfig.item is identical to filteredItems'
+    ).to.eql(testItems)
+  })
+  it('listItems computed property', function () {
+    expect(
+      controller.get('listItems')[0].id,
+      'listItems[0].id is set to 1'
+    ).to.eql(1)
+
+    expect(
+      controller.get('listItems')[1].id,
+      'listItems[1].id is set to 2'
+    ).to.eql(2)
+  })
+})
