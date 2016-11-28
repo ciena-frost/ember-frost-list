6fc7d624452400535ccb57b6b7132a7f7bc4e319
Jasonjys <jasonjys0511@gmail.com>
2016-11-28 15:53:40 -0500

    updated integation tests for frost-pagination

--- tests/integration/components/frost-pagination-test.js
+++ tests/integration/components/frost-pagination-test.js
@@ -1,12 +1,17 @@
 import { expect } from 'chai'
 import {
+  $hook,
+  initialize as initializeHook
+} from 'ember-hook'
+import {
   describeComponent,
   it
 } from 'ember-mocha'
 import hbs from 'htmlbars-inline-precompile'
 import {
   afterEach,
-  beforeEach
+  beforeEach,
+  describe
 } from 'mocha'
 import sinon from 'sinon'
 
@@ -20,6 +25,7 @@ describeComponent(
     let sandbox
 
     beforeEach(function () {
+      initializeHook()
       sandbox = sinon.sandbox.create()
     })
 
@@ -27,34 +33,218 @@ describeComponent(
       sandbox.restore()
     })
 
-    it('renders', function () {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.on('myAction', function(val) { ... });
-      // Template block usage:
-      // this.render(hbs`
-      //   {{#frost-pagination}}
-      //     template content
-      //   {{/frost-pagination}}
-      // `);
+    describe('default render state', function () {
+      beforeEach(function () {
+        this.set('actions', {
+          onChange: function () {}
+        })
 
-      this.set('actions', {
-        onChange: function () {
-          console.log('foo')
-        }
+        this.render(hbs`
+          {{frost-pagination
+            itemsPerPage=10
+            page=0
+            total=100
+            hook='myHook'
+            onChange=(action 'onChange')
+          }}
+        `)
       })
 
-      this.render(hbs`
-        {{frost-pagination
-          itemsPerPage=10
-          page=0
-          total=100
-          onChange=(action 'onChange')
-        }}
-      `)
-      expect(
-        this.$('.frost-pagination'),
-        'class frost-pagination is set'
-      ).to.have.length(1)
+      it('sets frost-pagination class', function () {
+        expect(
+          this.$('.frost-pagination'),
+          'class frost-pagination is set'
+        ).to.have.length(1)
+      })
+
+      it('shows correct pagination text', function () {
+        expect(
+          this.$('.frost-pagination-text').text().trim()
+        ).to.eql('1 to 10 of 100')
+      })
+
+      describe('hooks', function () {
+        it('sets "-first-page" hook', function () {
+          expect(
+            this.$($hook('myHook-first-page'))
+          ).to.have.length(1)
+        })
+
+        it('sets "-previous-page" hook', function () {
+          expect(
+            this.$($hook('myHook-previous-page'))
+          ).to.have.length(1)
+        })
+
+        it('sets "-next-page" hook', function () {
+          expect(
+            this.$($hook('myHook-next-page'))
+          ).to.have.length(1)
+        })
+
+        it('sets "-last-page" hook', function () {
+          expect(
+            this.$($hook('myHook-last-page'))
+          ).to.have.length(1)
+        })
+      })
+
+      describe('disables buttons on the left', function () {
+        it('disables "first page" button', function () {
+          expect(
+            this.$($hook('myHook-first-page')).prop('disabled')
+          ).to.eql(true)
+        })
+
+        it('disables "previous page" button', function () {
+          expect(
+            this.$($hook('myHook-previous-page')).prop('disabled')
+          ).to.eql(true)
+        })
+      })
+
+      describe('enables buttons on the right', function () {
+        it('enables "last page" button', function () {
+          expect(
+            this.$($hook('myHook-last-page')).prop('disabled')
+          ).to.eql(false)
+        })
+
+        it('enables "next page" button', function () {
+          expect(
+            this.$($hook('myHook-next-page')).prop('disabled')
+          ).to.eql(false)
+        })
+      })
+    })
+
+    describe('at page "11 to 20"', function () {
+      beforeEach(function () {
+        this.set('actions', {
+          onChange: function () {}
+        })
+
+        this.render(hbs`
+          {{frost-pagination
+            itemsPerPage=10
+            page=1
+            total=100
+            hook='myHook'
+            onChange=(action 'onChange')
+          }}
+        `)
+      })
+
+      it('shows correct pagination text', function () {
+        expect(
+          this.$('.frost-pagination-text').text().trim()
+        ).to.eql('11 to 20 of 100')
+      })
+
+      describe('enable all buttons', function () {
+        it('enables "first page" button', function () {
+          expect(
+            this.$($hook('myHook-first-page')).prop('disabled')
+          ).to.eql(false)
+        })
+
+        it('enables "previous page" button', function () {
+          expect(
+            this.$($hook('myHook-previous-page')).prop('disabled')
+          ).to.eql(false)
+        })
+
+        it('enables "next page" button', function () {
+          expect(
+            this.$($hook('myHook-next-page')).prop('disabled')
+          ).to.eql(false)
+        })
+
+        it('enables "last page" button', function () {
+          expect(
+            this.$($hook('myHook-last-page')).prop('disabled')
+          ).to.eql(false)
+        })
+      })
+    })
+
+    describe('at last page', function () {
+      beforeEach(function () {
+        this.set('actions', {
+          onChange: function () {}
+        })
+
+        this.render(hbs`
+          {{frost-pagination
+            itemsPerPage=10
+            page=9
+            total=100
+            hook='myHook'
+            onChange=(action 'onChange')
+          }}
+        `)
+      })
+
+      it('shows correct pagination text', function () {
+        expect(
+          this.$('.frost-pagination-text').text().trim()
+        ).to.eql('91 to 100 of 100')
+      })
+
+      describe('enables buttons on the left', function () {
+        it('enables "first page" button', function () {
+          expect(
+            this.$($hook('myHook-first-page')).prop('disabled')
+          ).to.eql(false)
+        })
+
+        it('enables "previous page" button', function () {
+          expect(
+            this.$($hook('myHook-previous-page')).prop('disabled')
+          ).to.eql(false)
+        })
+      })
+
+      describe('disables buttons on the right', function () {
+        it('disables "next page" button', function () {
+          expect(
+            this.$($hook('myHook-next-page')).prop('disabled')
+          ).to.eql(true)
+        })
+
+        it('disables "last page" button', function () {
+          expect(
+            this.$($hook('myHook-last-page')).prop('disabled')
+          ).to.eql(true)
+        })
+      })
+    })
+
+    describe('fires onChange closure action', function () {
+      let onChangeSpy
+
+      beforeEach(function () {
+        onChangeSpy = sandbox.spy()
+        this.on('onChangeAction', onChangeSpy)
+
+        this.render(hbs`
+          {{frost-pagination
+            itemsPerPage=10
+            page=0
+            total=100
+            hook='myHook'
+            onChange=(action 'onChangeAction')
+          }}
+        `)
+
+        this.$($hook('myHook-next-page')).trigger('click')
+      })
+
+      it('fires onChangeSpy', function () {
+        expect(
+          onChangeSpy.called,
+        ).to.eql(true)
+      })
     })
   }
 )
