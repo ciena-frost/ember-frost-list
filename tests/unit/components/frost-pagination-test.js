1dd56662f980f072bb644af6254b3151b77ad4cf
Jasonjys <jasonjys0511@gmail.com>
2016-11-30 11:44:02 -0500

    added computed property unit tests for frost-pagination

--- tests/unit/components/frost-pagination-test.js
+++ tests/unit/components/frost-pagination-test.js
@@ -1,4 +1,6 @@
 import { expect } from 'chai'
+import Ember from 'ember'
+const { run } = Ember
 import { describeComponent } from 'ember-mocha'
 import PropTypeMixin from 'ember-prop-types'
 import { beforeEach, describe, it } from 'mocha'
@@ -93,6 +95,136 @@ describeComponent(
         PropTypeMixin.detect(component),
         'PropTypeMixin Mixin is present'
       ).to.eql(true)
+    })
+
+    describe('_end computed property', function () {
+      it('is set to pageMax when it is NOT at the last page', function () {
+        run(() => {
+          component.set('itemsPerPage', 10)
+          component.set('page', 5)
+          component.set('total', 100)
+        })
+
+        const expectedPageMax = (component.get('page') + 1) * component.get('itemsPerPage')
+
+        expect(
+          component.get('_end'),
+        ).to.eql(expectedPageMax)
+      })
+
+      it('is set to total when it is at the last page', function () {
+        run(() => {
+          component.set('itemsPerPage', 10)
+          component.set('page', 9)
+          component.set('total', 100)
+        })
+
+        const total = component.get('total')
+
+        expect(
+          component.get('_end'),
+        ).to.eql(total)
+      })
+    })
+
+    describe('_isLeftDisabled computed property', function () {
+      it('is set to true when it is at the first page', function () {
+        run(() => component.set('page', 0))
+
+        expect(
+          component.get('_isLeftDisabled'),
+        ).to.eql(true)
+      })
+
+      it('is set to false when it is NOT at the first page', function () {
+        run(() => component.set('page', 5))
+
+        expect(
+          component.get('_isLeftDisabled'),
+        ).to.eql(false)
+      })
+    })
+
+    describe('_isRightDisabled computed property', function () {
+      it('is set to true when total is equal to 0', function () {
+        run(() => component.set('total', 0))
+
+        expect(
+          component.get('_isRightDisabled'),
+        ).to.eql(true)
+      })
+
+      it('is set to true at the last page', function () {
+        run(() => {
+          component.set('itemsPerPage', 10)
+          component.set('page', 9)
+          component.set('total', 100)
+        })
+
+        expect(
+          component.get('_isRightDisabled'),
+        ).to.eql(true)
+      })
+
+      it('is set to false NOT at the last page', function () {
+        run(() => { 
+          component.set('itemsPerPage', 10)
+          component.set('page', 5)
+          component.set('total', 100)
+        })
+
+        expect(
+          component.get('_isRightDisabled'),
+        ).to.eql(false)
+      })
+    })
+
+    describe('_offset computed property', function () {
+      it('is set to 0 when total is equal to 0', function () {
+        run(() => component.set('total', 0))
+
+        expect(
+          component.get('_offset'),
+        ).to.eql(0)
+      })
+
+      it('is set to correct off set of that page', function () {
+        run(() => {
+          component.set('itemsPerPage', 10)
+          component.set('page', 2)
+          component.set('total', 100)
+        })
+
+        const page = component.get('page')
+        const itemsPerPage = component.get('itemsPerPage')
+        const expectedOffset = page * itemsPerPage + 1
+
+        expect(
+          component.get('_offset'),
+        ).to.eql(expectedOffset)
+      })
+    })
+
+    describe('_paginationText computed property', function () {
+      it('is set to "0 results found" when total is equal to 0', function () {
+        run(() => component.set('total', 0))
+
+        expect(
+          component.get('_paginationText'),
+        ).to.eql('0 results found')
+      })
+
+      it('is set to "1 to 10 of 100" at frist page', function () {
+        run(() => {
+          component.set('itemsPerPage', 10)
+          component.set('page', 0)
+          component.set('total', 100)
+        })
+
+        expect(
+          component.get('_paginationText'),
+        ).to.eql('1 to 10 of 100')
+      })
     })
   }
 )
