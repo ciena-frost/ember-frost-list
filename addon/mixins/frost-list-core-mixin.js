9a1d4f436fdff7628d83c47ef00d739507ca6986
Quincy <qle@ciena.com>
2016-12-01 15:37:48 -0500

    minor bug fix

--- addon/mixins/frost-list-core-mixin.js
+++ addon/mixins/frost-list-core-mixin.js
@@ -11,9 +11,6 @@ import computed from 'ember-computed-decorators'
 export default Mixin.create({
   initListCoreMixin: on('init', function () {
     defineProperty(this, '_listItems', alias(get(this, 'listConfig.items')))
-    if (Ember.isNone(this.get('_listItems'))) {
-      this.set(get(this, 'listConfig.items'), [])
-    }
   }),
 
   @computed('_listItems.[]')
