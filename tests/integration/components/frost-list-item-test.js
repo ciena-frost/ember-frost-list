837c23ee9276494564cf3bad2e6985bcd0057d91
Steven Glanzer <sglanzer@ciena.com>
2017-01-15 22:38:55 -0500

    Merged with upstream

--- tests/integration/components/frost-list-item-test.js
+++ tests/integration/components/frost-list-item-test.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-/**
- * Integration test for the frost-list-item component
- */
-
-import {expect} from 'chai'
-import {describeComponent, it} from 'ember-mocha'
-import hbs from 'htmlbars-inline-precompile'
-import {$hook, initialize as initializeHook} from 'ember-hook'
-import wait from 'ember-test-helpers/wait'
-import {afterEach, beforeEach, describe} from 'mocha'
-import sinon from 'sinon'
-
-import {integration} from 'ember-frost-list/tests/helpers/ember-test-utils/describe-component'
-
-describeComponent(...integration('frost-list-item'), function () {
-  let sandbox
-
-  beforeEach(function () {
-    sandbox = sinon.sandbox.create()
-    initializeHook()
-  })
-
-  afterEach(function () {
-    sandbox.restore()
-  })
-
-  it('should have real tests', function () {
-    expect(true).to.equal(false)
-  })
-
-  describe('after render', function () {
-    beforeEach(function () {
-      this.setProperties({
-        myHook: 'myThing'
-      })
-
-      this.render(hbs`
-        {{frost-list-item
-          hook=myHook
-        }}
-      `)
-
-      return wait()
-    })
-
-    it('should have an element', function () {
-      expect(this.$()).to.have.length(1)
-    })
-
-    it('should be accessible via the hook', function () {
-      expect($hook('myThing')).to.have.length(1)
-    })
-  })
-})
-=======
 import { expect } from 'chai'
 import { describeComponent, it } from 'ember-mocha'
 import { afterEach, beforeEach, describe } from 'mocha'
@@ -168,4 +112,3 @@ describeComponent(
     })
   }
 )
->>>>>>> 6eef66e301094525ab7c7a0c4c4390538080546b
