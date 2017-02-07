7449b41faabdd0f76170a0558a25c3b24fb12906
Michael Carroll <juwara0@users.noreply.github.com>
2017-02-07 13:52:25 -0600

    fixed linting issue

--- tests/unit/helpers/floor-test.js
+++ tests/unit/helpers/floor-test.js
@@ -2,7 +2,6 @@ import {expect} from 'chai'
 import {floor} from 'ember-frost-list/helpers/floor'
 import {describe, it} from 'mocha'
 
-
 describe.skip('Unit / Helper / floor', function () {
   it('works for positive numbers', function () {
     let result = floor([42.8])
