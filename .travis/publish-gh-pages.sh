0f7c43fc93d139145f856133807cb0111f539ccf
Seena Rowhani <seenarowhani95@gmail.com>
2017-03-03 17:51:24 -0500

    fix(publish)

--- .travis/publish-gh-pages.sh
+++ .travis/publish-gh-pages.sh
@@ -3,13 +3,6 @@
 VERSION=`node -e "console.log(require('./package.json').version)"`
 TMP_GH_PAGES_DIR=.gh-pages-demo
 
-# We only want to deploy to gh-pages from "master"
-if [ "${TRAVIS_BRANCH}" != "master" ]
-then
-    echo "Skipping gh-pages publish for branch ${TRAVIS_BRANCH}"
-    exit 0
-fi
-
 ember build --prod
 git clone https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${TMP_GH_PAGES_DIR} > /dev/null 2>&1
 cd ${TMP_GH_PAGES_DIR}
