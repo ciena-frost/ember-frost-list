37c3b453fc7744b44b7e47987a2e70af59d9d627
dafortin@ciena.com <dafortin@ciena.com>
2017-04-18 13:28:05 -0400

    Added blueprint check

--- blueprints/ember-frost-list/index.js
+++ blueprints/ember-frost-list/index.js
@@ -1,15 +1,31 @@
+const blueprintHelper = require('ember-frost-core/blueprint-helper')
+
 module.exports = {
-  afterInstall: function () {
-    return this.addAddonsToProject({
-      packages: [
-        {name: 'ember-frost-core', target: '^1.5.1'},
-        {name: 'ember-frost-sort', target: '^7.0.0'},
-        {name: 'ember-math-helpers', target: '^2.0.0'},
-        {name: 'smoke-and-mirrors', target: 'github:ciena-frost/smoke-and-mirrors'}
-      ]
+  afterInstall: function (options) {
+    const addonsToAdd = [
+      {name: 'ember-frost-core', target: '^1.14.3'},
+      {name: 'ember-frost-sort', target: '^7.0.0'},
+      {name: 'ember-math-helpers', target: '^2.0.0'},
+      {name: 'smoke-and-mirrors', target: 'github:ciena-frost/smoke-and-mirrors'}
+    ]
+
+    // Get the packages installed in the consumer app/addon. Packages that are already installed in the consumer within
+    // the required semver range will not be re-installed or have blueprints re-run.
+    const consumerPackages = blueprintHelper.consumer.getPackages(options)
+
+    // Get the packages to install (not already installed) from a list of potential packages
+    return blueprintHelper.packageHandler.getPkgsToInstall(addonsToAdd, consumerPackages).then((pkgsToInstall) => {
+      if (pkgsToInstall.length !== 0) {
+        // Call the blueprint hook
+        return this.addAddonsToProject({
+          packages: pkgsToInstall
+        })
+      }
     })
   },
 
   normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter to us)
   }
 }
