6af86e5b4249fb3fb6fa0666e91a454bbf0e5404
Michael Carroll <juwara0@users.noreply.github.com>
2017-05-02 15:55:41 -0500

    updating to use pr-bumper maybe-check-coverage script

--- .travis/maybe-check-coverage.sh
+++ .travis/maybe-check-coverage.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-source $(npm root -g)/pr-bumper/.travis/is-bump-commit.sh
-
-if isBumpCommit
-then
-  echo "Skipping coverage check for version bump commit"
-  exit 0
-fi
-
 if [ "$TRAVIS_NODE_VERSION" != "6.9.1" ]
 then
-  echo "Skipping coverage check for TRAVIS_NODE_VERSION ${TRAVIS_NODE_VERSION}"
+  echo "Skipping version bump for TRAVIS_NODE_VERSION ${TRAVIS_NODE_VERSION}"
   exit 0
 fi
 
 if [ "$EMBER_TRY_SCENARIO" != "default" ]
 then
-  echo "Skipping coverage check for EMBER_TRY_SCENARIO ${EMBER_TRY_SCENARIO}"
+  echo "Skipping version bump for EMBER_TRY_SCENARIO ${EMBER_TRY_SCENARIO}"
   exit 0
 fi
 
-pr-bumper check-coverage
+$(npm root -g)/pr-bumper/.travis/maybe-check-coverage.sh
