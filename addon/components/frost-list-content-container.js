628c4e5a8620c0bf39b2a0055971eebce64e1a28
Steven Glanzer <sglanzer@ciena.com>
2017-06-01 12:48:36 -0400

    Removing animation

--- addon/components/frost-list-content-container.js
+++ addon/components/frost-list-content-container.js
@@ -7,7 +7,6 @@ const {isNone} = Ember
 import computed, {readOnly} from 'ember-computed-decorators'
 import {Component} from 'ember-frost-core'
 import {PropTypes} from 'ember-prop-types'
-import {animate} from 'liquid-fire'
 
 import layout from '../templates/components/frost-list-content-container'
 
@@ -50,26 +49,9 @@ export default Component.extend({
   @computed('pagination')
   paged (pagination) {
     return !isNone(pagination)
-  },
+  }
 
   // == Functions =============================================================
-
-  _transition () {
-    this.transition(
-      this.use(function (opts = {duration: 300}) {
-        if (this.oldElement) {
-          this.oldElement.css('opacity', 1)
-        }
-        if (this.newElement) {
-          this.newElement.css('opacity', 0)
-        }
-
-        return animate(this.oldElement, {opacity: 0}, opts).then(() => {
-          return animate(this.newElement, {opacity: 1}, opts)
-        })
-      })
-    )
-  }
 
   // == DOM Events ============================================================
 
