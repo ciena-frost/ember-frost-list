fd0da181f11d5f00b3b36b4c85d370170ee7d596
Quincy Le <lexuqing8@gmail.com>
2017-10-21 01:18:52 -0400

    fix component name issue in ember 2.8

--- addon/utils/get-component-name.js
+++ addon/utils/get-component-name.js
@@ -1,18 +1,18 @@
 import Ember from 'ember'
 const {get} = Ember
 
+function findComponentNameKey (keys) {
+  return keys.find((key) => {
+    return key.includes('__COMPONENT_PATH__') || key.includes('COMPONENT_PATH')
+  })
+}
+
+/**
+   *  Necessary to support Ember 2.8 when component name is not accessible via component.name
+   *  Ember: 2.8.3 and Ember: lts-2-8 assigns component name to different internal key.
+   *  @param {Object} component - Ember component instance
+   *  @returns {string} | component's name
+ */
 export default function getComponentName (component) {
-  let componentName = get(component, 'name')
-
-  // Necessary to support Ember 2.8-LTS
-  if (!componentName) {
-    componentName = Object.keys(component).reduce((accumulator, key) => {
-      if (key.indexOf('__COMPONENT_PATH__') > -1) {
-        accumulator = component[key]
-      }
-      return accumulator
-    }, '')
-  }
-
-  return componentName
+  return get(component, 'name') || component[findComponentNameKey(Object.keys(component))]
 }
