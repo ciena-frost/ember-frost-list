a9cdf1aa653d83e1a82ebde9d0628c9ee29be1ea
Michael Carroll <juwara0@users.noreply.github.com>
2018-01-24 12:17:09 -0600

    remove useLintTree ember-cli-mocha configuration from ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
-    'ember-cli-mocha': {
-      useLintTree: false
-    },
     sassOptions: {
       includePaths: [
       ]
