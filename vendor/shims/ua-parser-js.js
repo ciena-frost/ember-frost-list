42a7bfb53ccfe8dcca896b52c15e9088e469fd19
Michael Carroll <juwara0@users.noreply.github.com>
2018-01-25 10:23:46 -0600

    make vendor shim comply with linting

--- vendor/shims/ua-parser-js.js
+++ vendor/shims/ua-parser-js.js
@@ -1,15 +1,15 @@
 /* eslint-env node:true */
 /* globals define, self */
 
-(function() {
-  function vendorModule() {
-    'use strict';
+(function () {
+  function vendorModule () {
+    'use strict'
 
     return {
       'default': self['UAParser'],
-      __esModule: true,
-    };
+      __esModule: true
+    }
   }
 
-  define('ua-parser-js', [], vendorModule);
-})();
+  define('ua-parser-js', [], vendorModule)
+})()
